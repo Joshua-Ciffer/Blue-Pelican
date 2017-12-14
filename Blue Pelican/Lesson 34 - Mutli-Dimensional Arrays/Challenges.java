52eb9e02fa37c762154f5d31f37a5e4f79abaea9
Joshua-Ciffer <Joshua_Ciffer@Outlook.com>
2017-12-13 22:16:47 -0500

    Finished lesson 34 challenges.

--- Blue Pelican/Lesson 34 - Mutli-Dimensional Arrays/Challenges.java
+++ Blue Pelican/Lesson 34 - Mutli-Dimensional Arrays/Challenges.java
@@ -11,13 +11,15 @@ public abstract class Challenges {
 
 	public static void main(String[] args) {
 		do {	// Begin Main Program Loop
+			@SuppressWarnings("resource")
 			Scanner userInput = new Scanner(System.in) ;
 			short menuOption ;
-			System.out.println("-----Stupid Arrays Problem-----") ;
+			System.out.println("-----Stupid Array Problems-----") ;
 			System.out.println("(1) randomInts()") ;
 			System.out.println("(2) invert()") ;
 			System.out.println("(3) toCharArray()") ;
 			System.out.println("(4) doStuff()") ;
+			System.out.println("(5) Exit") ;
 			System.out.print("Enter an option: ") ;
 			try {
 				menuOption = userInput.nextShort() ;
@@ -27,7 +29,54 @@ public abstract class Challenges {
 				continue ;
 			}
 			switch (menuOption) {	// Begin Menu Switch
-				
+				case 1: {	// randomInts()
+					randomInts() ;
+					break ;
+				}
+				case 2: {	// invert()
+					try {
+						invert(fillArray()) ;
+					} catch (IllegalArgumentException e) {
+						System.out.println(e.getMessage()) ;
+					}
+					break ;
+				}
+				case 3: {	// toCharArray()
+					do {
+						System.out.print("How many words do you want to enter?: ") ;
+						short numWords ;
+						try {
+							numWords = userInput.nextShort() ;
+						} catch (InputMismatchException e) {
+							System.out.println("Enter a number please.") ;
+							userInput.next() ;
+							continue ;
+						}
+						String[] words = new String[numWords] ;
+						userInput.nextLine() ;
+						for (int i = 0 ; i < numWords ; i++) {
+							System.out.print("Enter word #" + (i + 1) + ": ") ;
+							words[i] = userInput.nextLine();
+						}
+						toCharArray(words) ; 
+						break ;
+					} while (true) ;
+					break ;
+				}
+				case 4: {	// doStuff()
+					// Change array values for doStuff() here. I didn't feel like implementing a way to create an int[][] off of user input.
+					int[][] x = {{2, 5, 6, 87, 23}, {6, 7, 7, 7, 20}, {1, 2, 5, 80, 4}} ;
+					doStuff(x) ;
+					break ;
+				}
+				case 5: {	// Exit
+					userInput.close() ; 
+					System.exit(0) ;
+				}
+				default: {
+					System.out.print("\nEnter an option you idiot.\n") ;
+					continue ;
+				}
 			}	// End Menu Switch
 		} while (true) ;	// End Main Program Loop
 	}
@@ -58,6 +107,8 @@ public abstract class Challenges {
 				invertedAry[i][j] = originalAry[j][i] ;
 			}
 		}
+		printArray(originalAry) ;
+		System.out.print("\n") ;
 		printArray(invertedAry) ;
 	}
 	
@@ -67,10 +118,10 @@ public abstract class Challenges {
 			charAry[i] = new char[strAry[i].length()] ;
 		}
 		for (int i = 0 ; i < charAry.length ; i++) {
+			int k = 0 ;
 			for (int j = 0 ; j < charAry[i].length ; j++) {
-				for (int k = 0 ; k < strAry[i].length() ; k++) {
-					charAry[i][j] = strAry[i].charAt(k) ;
-				}
+				charAry[i][j] = strAry[i].charAt(k) ;
+				k++ ;
 			}
 		}
 		printArray(strAry) ;
@@ -78,28 +129,54 @@ public abstract class Challenges {
 		return charAry ;
 	}
 	
+	public static void doStuff(int[][] ary) {
+		int[][] newAry = new int[ary.length][] ;
+		for (int i = 0 ; i < newAry.length ; i++) {
+			newAry[i] = new int[ary[i].length] ;
+		}
+		for (int i = 0 ; i < newAry.length ; i++) {
+			for (int j = 0 ; j < newAry[i].length ; j++) {
+				if (i == (newAry.length - 1)) {
+					newAry[i][j] = ary[i][j] ;
+				} else if (ary[i][j] > 80) {
+					newAry[i][j] = ary[i][0] ;
+				} else if ((ary[i][j] % 2) == 0) {
+					newAry[i][j] = ary[i + 1][j] ;
+				} else {
+					newAry[i][j] = ary[i][j] ;
+				}
+			}
+		}
+		printArray(ary) ;
+		System.out.print("\n") ;
+		printArray(newAry) ;
+	}
+	
 	private static void printArray(String[] strAry) {
 		for (int i = 0 ; i < strAry.length ; i++) {
-			System.out.print(strAry[i] + "/t") ;
+			System.out.print(strAry[i] + "\t") ;
 		}
+		System.out.print("\n") ;
 	}
 	
 	private static void printArray(char[][] charAry) {
 		for (int i = 0 ; i < charAry.length ; i++) {
 			for (int j = 0 ; j < charAry[i].length ; j++) {
-				System.out.print(charAry[i][j] + "/t") ;
+				System.out.print(charAry[i][j] + "\t") ;
 			}
-			System.out.print("/n") ;
+			System.out.print("\n") ;
 		}
+		System.out.print("\n") ;
 	}
 	
 	private static void printArray(int[][] intAry) {
 		for (int i = 0 ; i < intAry.length ; i++) {
 			for (int j = 0 ; j < intAry[i].length ; j++) {
-				System.out.print(intAry[i][j] + "/t") ;
+				System.out.print(intAry[i][j] + "\t") ;
 			}
 			System.out.print("\n") ;
 		}
+		System.out.print("\n") ;
 	}
 	
 	private static int[][] fillArray() {
