bcda4d318b7c12aa6b90f04035e3f3a2510e60b1
Joshua <Joshua@CYPHER-LAPTOP>
2018-01-05 01:09:39 -0500

    idk what i changed

--- Blue Pelican/Lesson 23 - Input From a Disk File/FileParser.java
+++ Blue Pelican/Lesson 23 - Input From a Disk File/FileParser.java
@@ -1,38 +1,44 @@
 // Joshua Ciffer 11/17/2017 //
 
-import java.io.File ;
-import java.io.FileNotFoundException ;
-import java.util.InputMismatchException ;
-import java.util.Scanner ;
-import java.util.ArrayList ;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class FileParser {
 
 	public static void main(String[] args) {
 
-		Scanner scanner = null ;
-		File txt = new File("E:\\Data.in") ;
-		ArrayList<String> list = new ArrayList<>() ;
+		File textFile = new File("C:\\Users\\Joshua\\Desktop\\File.txt")
+		OutputStream out = new OutputStream();
+		PrintStream print = new PrintStream(out);
+		
+		
+		Scanner scanner = null;
+		File txt = new File("E:\\Data.in");
+		ArrayList<String> list = new ArrayList<>();
 		try {
-			scanner = new Scanner(txt) ;
+			scanner = new Scanner(txt);
 			try {
-				int i = 0 ;
+				int i = 0;
 				while (scanner.hasNext()) {
-					list.add(i, scanner.next()) ;
-					System.out.println(i + list.get(i)) ;
-					i++ ;
+					list.add(i, scanner.next());
+					System.out.println(i + list.get(i));
+					i++;
 				}
-			} catch (InputMismatchException e) {}
+			} catch (InputMismatchException e) {
+			}
 		} catch (FileNotFoundException e) {
-			e.printStackTrace() ;
+			e.printStackTrace();
 		} finally {
 			for (String x : list) {
-				System.out.println(x) ;
+				System.out.println(x);
 			}
-			scanner.close() ;
-			txt.renameTo(new File("E:\\Date.in")) ;
-			txt.delete() ;
-				
+			scanner.close();
+			txt.renameTo(new File("E:\\Date.in"));
+			txt.delete();
+
 		}
 	}
 
